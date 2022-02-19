fa7ae042915f03c6d48ec5c98f8d92e2228c5e65
KronosGR <geo.elgeo@gmail.com>
2022-02-19 20:10:11 +0100

    configured Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
+  purge: {
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: false,
   content: [],
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
