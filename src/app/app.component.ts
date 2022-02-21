67a314fd639d6e40e3f9ffd6b09584e96a5fd6e5
KronosGR <geo.elgeo@gmail.com>
2022-02-21 13:26:29 +0100

    finding memory leak on modal service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ModalService } from './services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-clipz-practice';
+  showModal = true;
+
+  constructor(public modal: ModalService){}
+
+  ngOnInit() {
+    setInterval(() => (this.showModal = !this.showModal), 1000);
+  }
 }
