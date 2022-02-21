7d0fda758a8214300502434d3f159cf57efee7a9
KronosGR <geo.elgeo@gmail.com>
2022-02-21 19:55:57 +0100

    portal alternative

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -7,17 +7,15 @@ import { ModalService } from 'src/app/services/modal.service';
   styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
-  @Input() modalID =''
+  @Input() modalID = '';
 
-  constructor(public modal:ModalService) {
-
-  }
+  constructor(public modal: ModalService, public el: ElementRef) {}
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 
-  closeModal(){
+  closeModal() {
     this.modal.toggleModal(this.modalID);
   }
-
 }
