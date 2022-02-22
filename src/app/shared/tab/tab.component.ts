0c65cb966739801888ece272420b34b976405551
KronosGR <geo.elgeo@gmail.com>
2022-02-22 11:42:08 +0100

    retrieve the amount of tabs

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
-  styleUrls: ['./tab.component.css']
+  styleUrls: ['./tab.component.css'],
 })
 export class TabComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
