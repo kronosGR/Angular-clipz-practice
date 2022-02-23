2dc39c48875e89e5b8f5946647ce935346b86798
KronosGR <geo.elgeo@gmail.com>
2022-02-23 12:36:22 +0100

    binding form controls

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,12 +7,20 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  name = new FormControl('', [Validators.required]); // default value empty string
+  email = new FormControl('');
+  age = new FormControl('');
+  password = new FormControl('');
+  confirm_password = new FormControl('');
+  phoneNumber = new FormControl('');
+
   registerForm = new FormGroup({
-    name: new FormControl('', [Validators.required]), // default value empty string
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
-    phoneNumber: new FormControl(''),
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber,
   });
+
 }
