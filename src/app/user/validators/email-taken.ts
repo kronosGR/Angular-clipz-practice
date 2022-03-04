06b4d14e0bedaea5cd09f78d3d3e03fe661a4d12
KronosGR <geo.elgeo@gmail.com>
2022-03-04 19:54:57 +0100

    async validator for email

---
+++ src/app/user/validators/email-taken.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class EmailTaken implements AsyncValidator{
+  constructor(private auth: AngularFireAuth){
+
+  }
+  validate(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    throw new Error('Method not implemented.');
+  }
+}
