fe8a2cfc935047c04888f26f1ba5591ae7ae27a9
KronosGR <geo.elgeo@gmail.com>
2022-03-04 20:03:46 +0100

    finished email taken validator

--- src/app/user/validators/email-taken.ts
+++ src/app/user/validators/email-taken.ts
@@ -7,13 +7,15 @@ import {
 } from '@angular/forms';
 import { Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class EmailTaken implements AsyncValidator {
   constructor(private auth: AngularFireAuth) {}
 
-  validate(
+  validate = (
     control: AbstractControl
-  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
     return this.auth
       .fetchSignInMethodsForEmail(control.value)
       .then((response) => (response.length ? { emailTaken: true } : null));
