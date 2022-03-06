2d9c388c1f7f5652c4be5427ea71d826dcddc4ea
KronosGR <geo.elgeo@gmail.com>
2022-03-06 19:47:05 +0100

    query parameters

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
-  styleUrls: ['./manage.component.css']
+  styleUrls: ['./manage.component.css'],
 })
 export class ManageComponent implements OnInit {
-
-  constructor() { }
+  videoOrder = '1';
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params: Params) => {
+      this.videoOrder = params.sort === '2' ? params.sort : '1';
+    });
   }
 
+  sort(event: Event) {
+    const { value } = event.target as HTMLSelectElement;
+
+    this.router.navigateByUrl(`/manage?sort=${value}`);
+  }
 }
