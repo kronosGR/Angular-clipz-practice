bec6b3d28dd0cca0e5deb8ff3feb792956f067c2
KronosGR <geo.elgeo@gmail.com>
2022-03-07 12:13:43 +0100

    added route guards

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {
+  AngularFireAuthGuard,
+  redirectUnauthorizedTo,
+} from '@angular/fire/compat/auth-guard';
+
 import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from './upload/upload.component';
+
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/');
 
 const routes: Routes = [
   {
@@ -9,19 +16,23 @@ const routes: Routes = [
     component: ManageComponent,
     data: {
       authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome,
     },
+    canActivate: [AngularFireAuthGuard],
   },
   {
     path: 'upload',
     component: UploadComponent,
     data: {
-      authOnly: true,
+      autOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome,
     },
+    canActivate: [AngularFireAuthGuard],
   },
   {
     path: 'manage-clips',
-    redirectTo:'manage'
-  }
+    redirectTo: 'manage',
+  },
 ];
 
 @NgModule({
