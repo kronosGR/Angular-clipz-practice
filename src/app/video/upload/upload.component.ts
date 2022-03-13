d38c2b21c0278087dca41f9b3c579dcad53a6b8e
KronosGR <geo.elgeo@gmail.com>
2022-03-13 12:44:56 +0100

    canceling upload when redirected

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { Component, OnDestroy } from '@angular/core';
+import {
+  AngularFireStorage,
+  AngularFireUploadTask,
+} from '@angular/fire/compat/storage';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { last, switchMap } from 'rxjs';
 import { v4 as uuid } from 'uuid';
@@ -13,7 +16,7 @@ import IClip from '../../models/clip.model';
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css'],
 })
-export class UploadComponent implements OnInit {
+export class UploadComponent implements OnDestroy {
   isDragover = false;
   file: File | null = null;
   nextStep = false;
@@ -24,6 +27,7 @@ export class UploadComponent implements OnInit {
   percentage = 0;
   showPercentage = false;
   user: firebase.User | null = null;
+  task?: AngularFireUploadTask;
 
   title = new FormControl('', [Validators.required, Validators.minLength(3)]);
   uploadForm = new FormGroup({
@@ -38,7 +42,9 @@ export class UploadComponent implements OnInit {
     auth.user.subscribe((user) => (this.user = user));
   }
 
-  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.task?.cancel();
+  }
 
   storeFile($event: Event) {
     this.isDragover = false;
@@ -66,14 +72,14 @@ export class UploadComponent implements OnInit {
     const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}.mp4`;
 
-    const task = this.storage.upload(clipPath, this.file);
+    this.task = this.storage.upload(clipPath, this.file);
     const clipRef = this.storage.ref(clipPath);
 
-    task.percentageChanges().subscribe((progress) => {
+    this.task.percentageChanges().subscribe((progress) => {
       this.percentage = (progress as number) / 100;
     });
 
-    task
+    this.task
       .snapshotChanges()
       .pipe(
         last(),
