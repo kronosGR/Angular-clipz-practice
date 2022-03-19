d936f8c348a0506960e8b2cad5877bae7a186c08
KronosGR <geo.elgeo@gmail.com>
2022-03-19 06:33:19 +0100

    sort clips

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/fire/compat/firestore';
 import IClip from '../models/clip.model';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { map, of, switchMap } from 'rxjs';
+import { map, of, switchMap, BehaviorSubject, combineLatest } from 'rxjs';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Injectable({
@@ -16,7 +16,11 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
 export class ClipService {
   public clipsCollection: AngularFirestoreCollection<IClip>;
 
-  constructor(private db: AngularFirestore, private auth: AngularFireAuth, private storage: AngularFireStorage) {
+  constructor(
+    private db: AngularFirestore,
+    private auth: AngularFireAuth,
+    private storage: AngularFireStorage
+  ) {
     this.clipsCollection = db.collection('clips');
   }
 
@@ -24,14 +28,17 @@ export class ClipService {
     return this.clipsCollection.add(data);
   }
 
-  getUserClips() {
-    return this.auth.user.pipe(
-      switchMap((user) => {
+  getUserClips(sort$: BehaviorSubject<string>) {
+    return combineLatest([this.auth.user, sort$]).pipe(
+      switchMap((values) => {
+        const [user, sort] = values;
         if (!user) {
           return of([]);
         }
 
-        const query = this.clipsCollection.ref.where('uid', '==', user.uid);
+        const query = this.clipsCollection.ref
+          .where('uid', '==', user.uid)
+          .orderBy('timestamp', sort === '1' ? 'desc' : 'asc');
 
         return query.get();
       }),
@@ -45,11 +52,11 @@ export class ClipService {
     });
   }
 
-  async deleteClip(clip: IClip){
-    const clipRef = this.storage.ref(`clips/${clip.filename}`)
+  async deleteClip(clip: IClip) {
+    const clipRef = this.storage.ref(`clips/${clip.filename}`);
 
-    await clipRef.delete()
+    await clipRef.delete();
 
-    await this.clipsCollection.doc(clip.docID).delete()
+    await this.clipsCollection.doc(clip.docID).delete();
   }
 }
