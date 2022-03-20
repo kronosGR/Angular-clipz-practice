001d2d6e9ebd13b3c08efce2f155da730b441999
KronosGR <geo.elgeo@gmail.com>
2022-03-20 19:50:28 +0100

    save files in memory

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -51,7 +51,7 @@ export class UploadComponent implements OnDestroy {
     this.task?.cancel();
   }
 
-  storeFile($event: Event) {
+  async storeFile($event: Event) {
     this.isDragover = false;
 
     this.file = ($event as DragEvent).dataTransfer
@@ -60,6 +60,8 @@ export class UploadComponent implements OnDestroy {
     if (!this.file || this.file.type !== 'video/mp4') {
       return;
     }
+
+    await this.ffmpegService.getScreenshots(this.file)
 
     this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
     this.nextStep = true;
