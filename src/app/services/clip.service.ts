bb7efb8bf276f5c51756cf2bee4e0d485ba8eebf
KronosGR <geo.elgeo@gmail.com>
2022-03-22 12:34:43 +0100

    using resolvers

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -7,13 +7,26 @@ import {
 } from '@angular/fire/compat/firestore';
 import IClip from '../models/clip.model';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { map, of, switchMap, BehaviorSubject, combineLatest } from 'rxjs';
+import {
+  map,
+  of,
+  switchMap,
+  BehaviorSubject,
+  combineLatest,
+  Observable,
+} from 'rxjs';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ClipService {
+export class ClipService implements Resolve<IClip | null> {
   public clipsCollection: AngularFirestoreCollection<IClip>;
   pageClips: IClip[] = [];
   pendingReq = false;
@@ -21,7 +34,8 @@ export class ClipService {
   constructor(
     private db: AngularFirestore,
     private auth: AngularFireAuth,
-    private storage: AngularFireStorage
+    private storage: AngularFireStorage,
+    private router: Router
   ) {
     this.clipsCollection = db.collection('clips');
   }
@@ -94,5 +108,25 @@ export class ClipService {
     });
 
     this.pendingReq = false;
+  }
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): IClip | Observable<IClip | null> | Promise<IClip | null> | null {
+    return this.clipsCollection
+      .doc(route.params.id)
+      .get()
+      .pipe(
+        map((snapshot) => {
+          const data = snapshot.data();
+
+          if (!data) {
+            this.router.navigate(['/']);
+            return null;
+          }
+          return data;
+        })
+      );
   }
 }
